831e696722a40bf16b081a4a6c46a8d354e9e909
Delurg <81343546+Delurg@users.noreply.github.com>
2024-03-28 13:38:36 +0300

    Update task.py

--- program/task.py
+++ program/task.py
@@ -1,1 +1,15 @@
+class TaskPlanners:
+  def __init__(self):
+    # Инициализация планироващика задач
 
+  def CreateTask(self):
+    #Создание задачи
+
+  def ChangeTask(self, task):
+    #Изменение задачи
+
+  def NotificationTask(self):
+    # Оповещение о задаче
+
+  def SaveTask(self, task):
+    # Сохранение задачи
