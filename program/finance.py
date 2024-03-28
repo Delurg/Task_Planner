75453cbba4a0969608de74f62c838b73f5b4b089
Delurg <81343546+Delurg@users.noreply.github.com>
2024-03-28 13:44:37 +0300

    Update finance.py

--- program/finance.py
+++ program/finance.py
@@ -1,1 +1,15 @@
+class Finances:
+  def __init__(self):
+    # Инициализация класса финансовых расчётов
 
+  def CreateReport(self, report):
+    # Модуль создания финансовго отчёта
+
+  def SaveReport(self, report):
+    #Модуль сохранения финансового отчёта
+
+  def ChangeReport(self, report):
+    #Модуль изменения финансового отчёта
+
+  def ChooseReport(self,report):
+    #Модуль выбора операции для финансового отчёта
